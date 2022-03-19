2d2c9f04d41c9cf284a435b1dd5ffb4f0f84d0c0
anastassun <99546747+anastassun@users.noreply.github.com>
2022-03-19 16:03:59 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-BOT_API = "5196914876:AAHeANqoy4sOKsF04EjsZZhxQGruBIDKeks"
+BOT_API = "XX"
 PROXY = {'proxy_url': 'socks5://t3.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
